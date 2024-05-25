bc63f2dba41b370a4f4841021f5180c0f442378a
Robert Dallas Schoff <schoffdallas@gmail.com>
2024-05-25 01:47:33 -0700

    Added Player & Enemy Health

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
 
+class_name Enemy
+
+signal attack
+signal healthChanged
+
 #Enemy trait values
+@export var MAX_HEALTH = 100
+@onready var CURRENT_HEALTH: int = MAX_HEALTH
 var SPEED = 30 #30 pixels per sec
-@onready var TARGET = $"../GravityPlayer"
+@onready var TARGET = $"../Player"
 @onready var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var enemy_attack_area = $AttackArea
+var hit_cooldown = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	healthChanged.emit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move_character(delta)
+	attack_player()
 	
 # Handle moving character left and right on its own
 func move_character(delta):
@@ -20,3 +30,14 @@ func move_character(delta):
 		velocity.x = position.direction_to(TARGET.position).x * SPEED
 		velocity.y += GRAVITY * delta
 		move_and_slide()
+		
+func attack_player():
+	hit_cooldown -= 1
+	if enemy_attack_area.has_overlapping_areas() and hit_cooldown <= 0:
+		attack.emit()
+		hit_cooldown = 150
+		print("hit player!")
+
+func hit_by_player():
+	CURRENT_HEALTH -= 10
+	healthChanged.emit()
