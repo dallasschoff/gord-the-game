afb5678ba93d91a511861fc7aa684fa2d0c58335
Robert Dallas Schoff <schoffdallas@gmail.com>
2024-06-16 18:45:57 -0700

    Tween for HealthBar changes

--- Scripts/healthBarComponent.gd
+++ Scripts/healthBarComponent.gd
@@ -2,10 +2,13 @@ class_name HealthBar
 extends TextureProgressBar
 
 var HEALTH
+var tween
 
-func _process(delta):
-	value = HEALTH
-
-func update(health):
-	HEALTH = health
+func update(health): 
+	var difference = health - value
+	var chunk = difference * 0.75
+	var slow = difference * 0.25
+	value += chunk
+	tween = get_tree().create_tween()
+	tween.parallel().tween_property(self, "value", value + slow, 0.5)
 	print("Updating health for %s" % get_owner().name)
