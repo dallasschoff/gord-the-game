48068eb434926f055a64c236567200f132a5305b
Robert Dallas Schoff <schoffdallas@gmail.com>
2024-07-11 22:37:54 -0700

    Enemy attack and lunge

--- Scripts/States/enemy_attack.gd
+++ Scripts/States/enemy_attack.gd
@@ -1,10 +1,11 @@
 extends State
-class_name EnemyFollow
+class_name EnemyAttack
 
 @export var enemy: CharacterBody2D
 @export var move_speed := 60
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player: CharacterBody2D
+var can_attack = true
 
 func enter():
 	player = get_tree().get_first_node_in_group("Player")
@@ -13,11 +14,11 @@ func physics_update(delta: float):
 	var direction = player.global_position - enemy.global_position
 	
 	if direction.length() > 40:
-		enemy.velocity.x = direction.normalized().x * move_speed
-	else:
-		transitioned.emit(self, "attack")
-	
-	if direction.length() > 200:
-		transitioned.emit(self, "idle")
+		transitioned.emit(self, "follow")
+	elif direction.length() < 40 and can_attack:
+		#continue to attack player if in range
+		var lunge_direction = enemy.global_position.direction_to(player.global_position)
+		var lunge = lunge_direction * move_speed
+		enemy._attack(lunge)
 		
 	enemy.velocity.y += gravity * delta
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -5,7 +5,7 @@ class_name Enemy
 signal blocked
 
 #Enemy trait values
-var SPEED = 60 #60 pixels per sec
+var SPEED = 60
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 @export var raycasts: Node2D
@@ -15,14 +15,29 @@ var SPEED = 60 #60 pixels per sec
 @export var HeartPickup: PackedScene
 #Animation traits
 var hurting_cooldown = 0
+var attack_cooldown = 0
 var is_idle
 var is_walking
 var right_colliding = false
 var left_colliding = false
 var knockback = Vector2(0,0)
 var tween
+var lunge = Vector2(0,0)
 
 func _physics_process(delta):
+	if attack_cooldown > 0:
+		if animated_sprite.animation == "attack" and (animated_sprite.frame >= 5 and animated_sprite.frame <= 10):
+			move_and_slide()
+		if animated_sprite.animation == "attack" and animated_sprite.frame == 5:
+			tween = get_tree().create_tween()
+			tween.parallel().tween_property(self, "lunge", Vector2(0,0), 2)
+		if animated_sprite.animation == "attack" and (animated_sprite.frame >= 7 and animated_sprite.frame <= 9):
+			weapon.attack_area.set_deferred("disabled", false)
+		else:
+			weapon.attack_area.set_deferred("disabled", true)
+		attack_cooldown -= 1
+		return
+		
 	move_and_slide()
 	
 	if abs(velocity.x) > 0:
@@ -42,6 +57,8 @@ func _physics_process(delta):
 	if wall_raycast.is_colliding() or (is_on_floor() and !floor_raycast.is_colliding()):
 		blocked.emit()
 	
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _physics_process(delta):
 	#move_character(delta)
@@ -54,7 +71,7 @@ func _physics_process(delta):
 		#animated_sprite.play("idle")
 #
 #
-## Handle moving en emy left and right on its own
+# Handle moving enemy left and right on its own
 #func move_character(delta):
 	#if TARGET != null:
 		#velocity.x = position.direction_to(TARGET.position).x * SPEED
@@ -67,7 +84,7 @@ func _physics_process(delta):
 		#if velocity.x > 0:
 			#weapon.change_direction("right")
 			#animated_sprite.flip_h = true
-		#
+
 func _hit(attack: Attack):
 	is_walking = false
 	is_idle = false
@@ -81,12 +98,13 @@ func _hit(attack: Attack):
 	tween.parallel().tween_property(self, "knockback", Vector2(0,0), 0.75)
 	
 func _die():
-	animated_sprite.play("hurting")
+	animated_sprite.play("death")
 	var heart = HeartPickup.instantiate()
 	heart.position = Vector2(position.x, position.y-8)
 	get_node("..").add_child(heart)
 	queue_free()
-#
-#func _handle_animation_cooldowns():
-	#if hurting_cooldown > 0:
-		#hurting_cooldown -= 1
+	
+func _attack(lunge_movement):
+	animated_sprite.play("attack")
+	attack_cooldown = 24
+	lunge = lunge_movement
