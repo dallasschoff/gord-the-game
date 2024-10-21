8bf2c3ed3ee343322514b8ed35d99ade30ec010d
Krieglliam <billyjr129@gmail.com>
2024-10-21 12:15:24 -0400

    Adding comments to player.gd

--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -3,18 +3,21 @@ class_name Player
 
 signal died
 
+#Player Attributes
 @export var speed := 600
 @export var max_speed := 200
 @export var jump_strength := 350
 @export var double_jump_multiplier := 1.2
 @export var stop_force := 1000
 @export var double_jumps := 1
+#Housekeeping
 @export var weapon: Weapon
 @export var log_movement := false
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var animated_sprite_puff = $AnimatedSprite2D/AnimatedSpritePuff
 @onready var landing_ray1 = $LandingRay1
 @onready var landing_ray2 = $LandingRay2
+#Player Conditions
 var jumps_made := 0
 var double_jumps_made := 0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -23,12 +26,14 @@ var attacking_cooldown = 0
 var crouching_cooldown = 0
 var landing_window = 0
 var hurting_cooldown = 0
+#Coyote Time
 var coyote = false
 var last_floor = false
 var coyote_timer: Timer
+#Running attack properties
 var running_attack_boost = false
 var running_attack_timer: Timer
-
+#Player knockback
 var knockback = Vector2(0,0)
 var knockbackTween
 var dead = false
@@ -42,12 +47,14 @@ var ground_level
 @export var PBMeteorScene: PackedScene
 
 func _ready():
+	#Coyote Time
 	coyote_timer = Timer.new()
 	add_child(coyote_timer)
 	coyote_timer.one_shot = true
 	coyote_timer.wait_time = 0.1
 	coyote_timer.connect("timeout", _on_coyote_timer_timeout)
 	
+	#Running attack
 	running_attack_timer = Timer.new()
 	add_child(running_attack_timer)
 	running_attack_timer.one_shot = true
@@ -59,6 +66,7 @@ func _ready():
 func _physics_process(delta):
 	if dead:
 		return
+	#Player States
 	var is_in_cooldown := attacking_cooldown > 0 or landing_window > 0 or crouching_cooldown > 0 or hurting_cooldown > 0
 	var is_landing = (landing_ray1.is_colliding() or landing_ray2.is_colliding()) and jumps_made >= 1 and velocity.y > 0.0 and !is_on_floor()
 	var is_free_falling = velocity.y > 200.0 and !is_on_floor() and jumps_made == 0
@@ -93,9 +101,6 @@ func _physics_process(delta):
 		world.add_child(pb_meteor_instance)
 		meteor_sprite.play("pb meteor")
 	
-		#if meteor_sprite.animation_finished:
-			#explosion_sprite.play("explosion")
-	
 	#Can only move if not hurting or attacking
 	if hurting_cooldown == 0 and attacking_cooldown == 0 and crouching_cooldown == 0:
 		_horizontal_direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -124,7 +129,7 @@ func _physics_process(delta):
 	
 	#All of the "is_" variables used to be here
 	
-		#Small speed boost / slide to running attack (I want need to relocate this)
+	#Small speed boost / slide to running attack (May want to relocate this)
 	if Input.is_action_just_pressed("attack") and is_moving:
 		print("running attack boost")
 		running_attack_boost = true
@@ -133,19 +138,23 @@ func _physics_process(delta):
 		if velocity.x > 100: velocity.x = 300
 		if velocity.x < -100: velocity.x = -300
 	
+	#Coyote Time
 	if !is_on_floor() and last_floor and jumps_made == 0:
 		#print("coyote time")
 		coyote = true
 		coyote_timer.start()
 	last_floor = is_on_floor()
 	
+	#Apply jump physics
 	if is_jumping:
 		jumps_made += 1
 		velocity.y = -jump_strength
+	#Apply double jump physics
 	elif is_double_jumping:
 		if jumps_made <= double_jumps:
 			velocity.y = -jump_strength * double_jump_multiplier
 		double_jumps_made += 1
+	#Reset jump counters
 	elif is_on_floor():
 		jumps_made = 0
 		double_jumps_made = 0
@@ -157,10 +166,11 @@ func _physics_process(delta):
 		landing_window = 12
 	if is_crouching:
 		crouching_cooldown = 20
-		
+	
 	#drop through platforms
 	set_collision_mask_value(2, false) if Input.is_action_pressed("crouch") else set_collision_mask_value(2, true)
 	
+	#Attack hitbox enable / disable
 	if animated_sprite.animation == "attack" and (animated_sprite.frame == 3 or animated_sprite.frame == 4):
 		weapon.attack_area.set_deferred("disabled", false)
 	elif animated_sprite.animation == "running_attack" and animated_sprite.frame >= 2 and animated_sprite.frame <= 5:
@@ -168,14 +178,17 @@ func _physics_process(delta):
 	else:
 		weapon.attack_area.set_deferred("disabled", true)
 	
+	#Aplly knockback physics
 	velocity = velocity + knockback
 	_update_ground_position()
 	last_ground_position = position.y
 	move_and_slide()
 	
+	#Dallas?
 	if hurting_cooldown > 0:
 		return
 	
+	#Handle sprites and hitbox direction changes
 	if Input.is_action_pressed("move_left") and attacking_cooldown == 0 and hurting_cooldown == 0:
 		animated_sprite.flip_h = true
 		weapon.change_direction("left")
@@ -186,12 +199,13 @@ func _physics_process(delta):
 		animated_sprite_puff.position.x = -2.33
 		weapon.change_direction("right")
 	
+	#Handle sprite puff flip/position when standing still
 	if velocity.x == 0 and animated_sprite.flip_h == false:
 		animated_sprite_puff.position.x = -2.33
-
 	if velocity.x == 0 and animated_sprite.flip_h == true:
 		animated_sprite_puff.position.x = 2
 	
+	#Player animations
 	if is_attacking_running and attacking_cooldown == 0:
 		animated_sprite.play("running_attack")
 		attacking_cooldown = 36
@@ -218,6 +232,7 @@ func _physics_process(delta):
 	elif is_idling:
 		animated_sprite.play("idle")
 	
+	#Movement log
 	if log_movement:
 		print(animated_sprite.animation)
 
